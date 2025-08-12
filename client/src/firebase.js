d5072c2416eca489b6d9621a7ac91f5c887aff4b
Aayush Paul <aayushpaul01@gmail.com>
2025-08-12 17:09:48 -0400

    secrets changes

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyoVDfroag3F9vH_Ls_3vad55weubEyDo",
-  authDomain: "fluency-app-463902.firebaseapp.com",
-  projectId: "fluency-app-463902",
-  storageBucket: "fluency-app-463902.firebasestorage.app",
-  messagingSenderId: "267373318894",
-  appId: "1:267373318894:web:276a7fa3fafb682fb199ff",
-  measurementId: "G-QPPCMEB26S"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
